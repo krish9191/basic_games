ee53e92f5e8ed1bd36f7bd34456d2c8794c96a8f
krishna <contact.krishnatiwari@gmail.com>
2021-05-19 15:27:33 +0200

    validate, if same player plays at other player turn

--- tic_tac_toe/tictactoe_run.py
+++ tic_tac_toe/tictactoe_run.py
@@ -1,4 +1,5 @@
 from tictactoe_manager import is_all_0, is_all_1, is_diagonal_0, is_diagonal_1, create_square_matrix, matrix_transpose
+import keyboard
 
 
 def tic_tac_toe():
@@ -6,13 +7,23 @@ def tic_tac_toe():
     matrix = create_square_matrix(n)
     left_diagonal = []
     right_diagonal = []
+    last_value = None
     for entries in range(n * n):
         row, column, value = map(int, input().split())
+        if last_value == value:
+            if last_value == 0:
+                print(" it's 1's turn")
+            else:
+                print(" it's 0's turn")
+        # if keyboard.is_pressed('enter'):
+        #     return 'GAME TERMINATED'
         if row + column == len(matrix) - 1:
             right_diagonal.append(value)
         if row == column:
             left_diagonal.append(value)
         matrix[row][column] = value
+        last_value = value
+
         t_matrix = matrix_transpose(matrix)
         if any([is_all_0(matrix), is_diagonal_0(left_diagonal, right_diagonal, n), is_all_0(t_matrix)]):
             print('GAME OVER')
